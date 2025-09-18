8b41d75338b1b9e1a897be7194126f359949a933
Farida-Ruslanovna <rus.farida@icloud.com>
2025-09-18 23:43:19 +0300

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -32,8 +32,9 @@ bool User::saveToFile(const std::string& filename) const {
 
     // Установка прав доступа 600
     if (chmod(filename.c_str(), S_IRUSR | S_IWUSR) != 0) {
-        // Можно логировать ошибку или вернуть false
+    
         return false;
     }
     return true;
+
 }
